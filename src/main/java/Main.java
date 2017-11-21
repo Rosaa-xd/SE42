236483f0945ba8ceb653e0eb66931bc22a6d964c
Shadowphoenix <github@shadowphoenix.nl>
2017-11-21 10:49:46 +0100

    final touchup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         EntityManager em = PersistenceManager.INSTANCE.getEntityManager();
-        testUpdate(em);
     }
 
     private static void testInsert(EntityManager em) {
